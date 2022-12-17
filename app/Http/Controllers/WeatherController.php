8d43369c13edc0fd1150cb0cec6c9667194490b7
saaa-padae <brick2crosslink@gmail.com>
2022-12-18 00:51:54 +0900

    [add] weathercontroller.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    public function index() {
+    public function index($index) {
         
         $cityName = 'Shimonoseki';
         $apiKey = 'aca786dd36879c4ba6c8dd591e5550f2';
@@ -21,7 +21,13 @@ class WeatherController extends Controller
 
         $data = $response->getBody();
         $data = json_decode($data, true);
+        $json = response()->json($data);
         
-        return response()->json($data);
+        return array($data['list'][$index]['dt_txt'],$data['list'][$index]['weather'][$index]['description'],$data['list'][$index]['main']['temp'],$data['list'][$index]['main']['pressure']);
     }
 }
+
+// list > dt_txt : 予測されるデータの時間、ISO、UTC
+// list > weather > description : 気象情報詳細
+// list > main > temp : 温度
+// list > main > pressure : 大気圧
